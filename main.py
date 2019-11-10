92ad839df13ce60a6d8c3495a902bdb14fec7953
Gabriel Guimarães Carvalho <gbrcarvalho@gmail.com>
2019-11-10 13:43:18 -0300

    Updates 2

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pylab import rcParams
-from methods import bollinger,exp_mov_average
+from methods import *
 import seaborn as sns
 rcParams['figure.figsize'] =15, 6
 sns.set()
 
-petr = pd.read_csv("PETR4.csv").dropna()
+petr = pd.read_csv("PETR4.csv")
+petr.dropna(inplace=True)
+petr.reset_index(drop=True,inplace=True)
+
+hi,lo,opn,close = gen_all_data(petr)
 
 
 # ########################## BOLLINGER BANDS ##################################################
-mu,up,low = bollinger(petr,"Close",5)
-plt.figure(1)
-plt.plot(mu,label="Moving Average")
-plt.plot(petr["Close"],label="Close")
-x_axis = petr["Close"].index.get_level_values(0)
-plt.fill_between(x_axis, up, low, color='silver')
-plt.legend()
-plt.plot()
-plt.show()
-plt.close(1)
-# ###################### EMA ################################################################
-exp = exp_mov_average(petr,"Close",5)
-plt.figure(2)
-plt.plot(petr["Close"],label="close")
-plt.plot(exp, label="EMA 5")
-plt.show()
-plt.close(2)
+# mu,up,low = bollinger(close,5)
+# plt.figure(1)
+# plt.plot(mu,label="Moving Average")
+# plt.plot(close,label="Close")
+# x_axis = close.index.get_level_values(0)
+# plt.fill_between(x_axis, up, low, color='silver')
+# plt.legend()
+# plt.plot()
+# plt.show()
+# plt.close(1)
+# ####################### EMA ################################################################
+# exp = exp_mov_average(close,5)
+# plt.figure(2)
+# plt.plot(close,label="close")
+# plt.plot(exp, label="EMA 5")
+# plt.show()
+# plt.close(2)
+
+close_windows = get_windows(close,5)
+print(close[close.index==100])
+print(close_windows[95])
--- methods.py
+++ methods.py
@@ -1,16 +1,41 @@
 import pandas as pd
 import numpy as np
 
-def bollinger(df,col,window):
-    mu = df[col].rolling(window=window).mean()
-    std = df[col].rolling(window=window).std()
+def gen_all_data(df):
+    High = df["High"]
+    Low = df["Low"]
+    Open = df["Open"]
+    Close = df["Close"]
+    return High,Low,Open,Close
+
+
+def bollinger(data,window):
+    mu = data.rolling(window=window).mean()
+    std = data.rolling(window=window).std()
     upper_band = mu + 2*std
     lower_band = mu -2*std
     return mu, upper_band, lower_band
 
 
-def exp_mov_average(df,col,window):
-    exp = df[col].ewm(span = window, adjust = False).mean()
+def exp_mov_average(data,window):
+    exp = data.ewm(span = window, adjust = False).mean()
     return exp
 
 
+def get_windows(data,window_size):
+    windows = []
+    for i in range(len(data)-window_size+1):
+        windows.append(data[i: i + window_size])
+    return windows
+
+"""
+To Do list:
+
+- Generate all inputs (table I)
+- MLP
+
+
+x_train = [....] 33 values from table I
+y_train = max,min of current day (???)
+
+"""
